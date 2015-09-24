864c5ead6ba1d49353f1a7753b13dc03ab814f4b
fryme <oleg.fry@gmail.com>
2015-09-24 19:25:55 +0300

    week 2 initial commit

---
+++ week2/assignment/src/Deque.java
@@ -0,0 +1,59 @@
+
+import java.util.Iterator; 
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+    
+    //construct an empty deque
+    public Deque()
+    {
+        
+    }
+
+    // is the deque empty?
+    public boolean isEmpty()
+    {
+        return true;
+    }
+    // return the number of items on the deque
+    public int size()
+    {
+        return 0;
+    }
+    
+    // add the item to the front
+    public void addFirst(Item item)
+    {
+        
+    }
+    
+    // add the item to the end
+    public void addLast(Item item)
+    {
+        
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst()
+    {
+        
+    }
+    
+    // remove and return the item from the end
+    public Item removeLast()
+    {
+        
+    }
+    
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator()
+    {
+        return new Iterator<Item>();
+    }
+    
+    // unit testing
+    public static void main(String[] args)
+    {
+        
+    }
+}
